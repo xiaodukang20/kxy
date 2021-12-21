e7bdc56d598b5f730990f5961697a8065fa05c56
xiaodukang20 <xiaodukang20@126.com>
2021-12-21 14:31:24 +0800

    .

--- src/Constants/ErrorCode.php
+++ src/Constants/ErrorCode.php
@@ -1,8 +1,11 @@
 <?php
 declare(strict_types=1);
+
 namespace Xiaodukang20\Kxy\Constants;
+
 use Hyperf\Constants\AbstractConstants;
 use Hyperf\Constants\Annotation\Constants;
+
 /**
  * @Constants
  */
---
+++ src/Exception/BusinessException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Xiaodukang20\Kxy\Exception;
+
+use Hyperf\Server\Exception\ServerException;
+
+class BusinessException extends ServerException
+{
+}
